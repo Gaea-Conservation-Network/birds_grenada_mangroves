5d57c4d39da6a45090778d441a310d0b14f94c72
Zoya Buckmire <buckmirz@myumanitoba.ca>
2021-09-10 12:08:53 -0400

    Code adjusted and figures remade without 2021 new plots

--- scripts/ivi.R
+++ scripts/ivi.R
@@ -1,10 +1,10 @@
 library(janitor)
 library(tidyverse)
 library(lubridate)
-library(BiodiversityR)
+# library(BiodiversityR)
 library(ggplot2)
 
-veg <- read.csv("data/Veg data for ivi.csv")
+veg <- read.csv("data/Veg data for ivi updated.csv")
 veg <- janitor::clean_names(veg)
 veg$date <- dmy(veg$date)
 veg$site <- as.factor(veg$site)
@@ -13,6 +13,7 @@ veg$plot <- as.factor(veg$plot)
 veg <- mutate(veg, plotID = str_c(site, transect, plot, sep = "0"))
 veg$plotID <- as.factor(veg$plotID)
 
+veg <- select(veg, -red_seedlings, - black_seedlings, -white_seedlings, -notes)
 
 #basal area by size classes, calculated separately
 a <- 2.01e-05
@@ -22,7 +23,7 @@ d <- 0.018132763
 e <- 0.0322394
 
 veg_long <- pivot_longer(veg, c(-site, -date, -start_time, -end_time, -transect, 
-                                -plot, -plotID, -redox_depth_cm), 
+                                -plot, -position, -plotID, -redox_depth_cm), 
                          names_to = "size", values_to = "count")
 
 size_a <- filter(veg_long, size %in% c("red_a", "white_a", "black_a"))
@@ -56,12 +57,12 @@ veg_spp$species <- as.factor(veg_spp$species)
 
 veg_all <- dplyr::select(veg_spp, forest = site, plotID, species, count, basal)
 
-importancevalue(veg_all, site = 'plotID', species = 'species', count = 'count', 
-               basal = 'basal', factor = 'forest', level = 'Levera')
+# importancevalue(veg_all, site = 'plotID', species = 'species', count = 'count', 
+               # basal = 'basal', factor = 'forest', level = 'Levera')
 
-importancevalue.comp(veg_all, site="plotID", species="species",
-                     count="count", basal="basal",
-                     factor="forest")
+# importancevalue.comp(veg_all, site="plotID", species="species",
+                     # count="count", basal="basal",
+                     # factor="forest")
 #ivi code consistently returning errors: "Error in if (xi > xj) 1L else -1L : missing value where TRUE/FALSE needed
 # In addition: Warning message:
 #   In Ops.factor(xi, xj) : '>' not meaningful for factors"
@@ -77,7 +78,9 @@ total_basal <- sum(veg_all$basal)
 
 # - Freqr in % = (frequency of a species / sum-frequency of all species) x 100
 total_plots <- n_distinct(veg_all$plotID)
-sum_freq <- 0.4 + 0.778 + 0.533 #individual frequencies from summary below
+summary <- veg_all %>% group_by(species) %>%
+  summarize(freq = n_distinct(plotID)/total_plots)
+sum_freq <- sum(summary$freq)
 
 summary <- veg_all %>% group_by(species) %>%
   summarize(density = sum(count), reldensity = (density/total_no)*100, 
@@ -85,15 +88,13 @@ summary <- veg_all %>% group_by(species) %>%
             freq = n_distinct(plotID)/total_plots, relfreq = (freq/sum_freq)*100, 
             ivi = reldensity + reldom + relfreq)
 
-write.csv(summary, "ivi calc by species.csv")
-
 plot <- summary %>% ggplot()+
-  geom_col(aes(x = species, y = ivi, fill = species))+
+  geom_col(aes(x = species, y = ivi, fill = species), fill = c("black", "red", "gray"))+
   theme_bw()+
   labs(x = "Mangrove species", y = "Importance Value Index")
 print(plot)
 
-ggsave("outputs/overall ivi plot.png", plot, width = 160, height = 100, unit = "mm")
+ggsave("figures/updated overall ivi plot.png", plot, width = 160, height = 100, unit = "mm")
 
 sites <- veg_all %>% group_by(site) %>%
   summarize(total_no = sum(count), 
@@ -102,45 +103,56 @@ sites <- veg_all %>% group_by(site) %>%
 
 Conf <- filter(veg_all, site %in% "Conference")
 confsummary <- Conf %>% group_by(species) %>%
-  summarize(density = sum(count), reldensity = (density/34)*100, 
-            dominance = sum(basal), reldom = (dominance/0.5204608)*100, 
-            freq = n_distinct(plotID)/6, relfreq = (freq/1.332)*100, 
+  summarize(freq = n_distinct(plotID)/6)
+sum_freq = sum(confsummary$freq)
+confsummary <- Conf %>% group_by(species) %>%
+  summarize(density = sum(count), reldensity = (density/42)*100, 
+            dominance = sum(basal), reldom = (dominance/0.689957)*100, 
+            freq = n_distinct(plotID)/6, relfreq = (freq/sum_freq)*100, 
             ivi = reldensity + reldom + relfreq)
 confsummary <- mutate(confsummary, site = "Conference")
 
 Lev <- filter(veg_all, site %in% "Levera")
 levsummary <- Lev %>% group_by(species) %>%
+  summarize(freq = n_distinct(plotID)/21)
+sum_freq = sum(levsummary$freq)
+levsummary <- Lev %>% group_by(species) %>%
   summarize(density = sum(count), reldensity = (density/229)*100, 
             dominance = sum(basal), reldom = (dominance/3.8554640)*100, 
-            freq = n_distinct(plotID)/21, relfreq = (freq/1.7143)*100, 
+            freq = n_distinct(plotID)/21, relfreq = (freq/sum_freq)*100, 
             ivi = reldensity + reldom + relfreq)
 levsummary <- mutate(levsummary, site = "Levera")
 
 Hart <- filter(veg_all, site %in% "Mt. Hartman")
 hartsummary <- Hart %>% group_by(species) %>%
-  summarize(density = sum(count), reldensity = (density/104)*100, 
-            dominance = sum(basal), reldom = (dominance/0.3878061)*100, 
-            freq = n_distinct(plotID)/6, relfreq = (freq/1.833)*100, 
+  summarize(freq = n_distinct(plotID)/6)
+sum_freq = sum(hartsummary$freq)
+hartsummary <- Hart %>% group_by(species) %>%
+  summarize(density = sum(count), reldensity = (density/109)*100, 
+            dominance = sum(basal), reldom = (dominance/0.403547)*100, 
+            freq = n_distinct(plotID)/6, relfreq = (freq/sum_freq)*100, 
             ivi = reldensity + reldom + relfreq)
 hartsummary <- mutate(hartsummary, site = "Mt. Hartman")
 
 West <- filter(veg_all, site %in% "Westerhall")
 westsummary <- West %>% group_by(species) %>%
-  summarize(density = sum(count), reldensity = (density/242)*100, 
-            dominance = sum(basal), reldom = (dominance/1.8445155)*100, 
-            freq = n_distinct(plotID)/12, relfreq = (freq/1.833)*100, 
+  summarize(freq = n_distinct(plotID)/12)
+sum_freq = sum(westsummary$freq)
+westsummary <- West %>% group_by(species) %>%
+  summarize(density = sum(count), reldensity = (density/284)*100, 
+            dominance = sum(basal), reldom = (dominance/1.851027)*100, 
+            freq = n_distinct(plotID)/12, relfreq = (freq/sum_freq)*100, 
             ivi = reldensity + reldom + relfreq)
 westsummary <- mutate(westsummary, site = "Westerhall")
 
 sitesummary <- bind_rows(confsummary, levsummary, hartsummary, westsummary)
 
-write.csv(sitesummary, "ivi calc by site.csv")
-
 iviplot <- sitesummary %>% ggplot()+
-  geom_col(aes(x = species, y = ivi, fill = species))+
+  geom_col(aes(x = species, y = ivi, fill = species), 
+           fill = c("black", "red", "gray", "black", "red", "gray", "black", "red", "gray", "black", "red", "gray"))+
   facet_wrap(.~site)+
   theme_bw()+
   labs(x = "Mangrove species", y = "Importance Value Index")
 print(iviplot)
 
-ggsave("outputs/ivi plots by site.png", iviplot, width = 160, height = 100, unit = "mm")
+ggsave("figures/updated ivi plot by site.png", iviplot, width = 160, height = 100, unit = "mm")
